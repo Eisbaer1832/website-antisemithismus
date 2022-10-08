bbdee5dda8a02b2ec3db5ce30b03ac8859a8d994
eisbaer1832 <eisbaer1832@gmail.com>
2022-10-08 16:33:30 +0000

    content added

--- public/js/app_dt.js
+++ public/js/app_dt.js
@@ -42,9 +42,33 @@ document.getElementById("click6").onclick = function() {
     });
 }
 
+document.getElementById("click7").onclick = function() {
+    document.querySelector('#content7').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+document.getElementById("click8").onclick = function() {
+
+    document.querySelector('#content8').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+document.getElementById("click9").onclick = function() {
+    document.querySelector('#content9').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+
 document.getElementById("click10").onclick = function() {                                      document.querySelector('#content10').scrollIntoView({                                          behavior: 'smooth'                                                                    });                                                                                   }
 
-
+document.getElementById("click11").onclick = function() {
+    document.querySelector('#content11').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
 function to_top()
 {
     document.querySelector('#top').scrollIntoView({
